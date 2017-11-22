ad700d781c8a109ce5167e2469d423f836b43fbc
Ryan DeCook <ryandecook@gmail.com>
2017-11-22 16:15:07 -0600

    started getItems function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,9 +2,34 @@ console.log('JS loaded');
 
 $(document).ready(function(){
     console.log('JQ ready');
-    
+    getItems();
 
 
 
 })//end document ready
 
+
+function getItems(){
+    console.log('in getItems');
+    $.ajax({
+        url: '/todo',
+        type: 'GET',
+        success: function (response){
+            console.log('got some items!', response);
+            $('#todoList').empty();
+            for (let i = 0; i < response.length; i++) {
+                var todo = response[i];
+                var $newTodoItem = $('<li>' + todo.item + '</li>');
+                
+                //create completed button for each item
+
+                //create delete button for each item
+                
+                //append todo items to DOM
+
+            }
+            
+        }
+    })
+    
+}
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -4,8 +4,31 @@ var router = express.Router();
 var pool = require('../modules/pool');
 
 
-//GET
-
+// begin GET todo
+router.get('/', function(req,res){
+    //Attempt to connect to database
+    pool.connect(function(errorConnectingToDatabase, client, done){
+        if(errorConnectingToDatabase){
+            //There was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // We connected to the database!
+            //Now, we're going to GET things from the DB
+            client.query('SELECT * FROM todo_list ORDER BY id;', function(errorMakingQuery, result){
+                done();
+                if(errorMakingQuery){
+                    //Query failed.  Did you test it in Postico?
+                    //Log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else{
+                    res.send(result.rows);
+                }
+            });
+        }
+    });
+});//end GET todo
 
 
 
