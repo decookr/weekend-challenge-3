bbe8fd563d0333920f04548ece5f349f4372b5ea
Ryan DeCook <ryandecook@gmail.com>
2017-11-22 15:36:59 -0600

    added module for pool and separate todo route

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-//add routes here if desired var routename = require('./routes/routename');
+var todo = require('./routes/todo');
 
 var port = 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
-// add app.use('/routenamehere', routenamehere); if using routes
+app.use('/todo', todo);
 
 app.listen(port, function(){
     console.log('server is listening on port', port);; 
