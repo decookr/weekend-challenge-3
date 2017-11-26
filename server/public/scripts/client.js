196e1d1df26fb3203eb50f9e3a75f465661e2290
Ryan DeCook <ryandecook@gmail.com>
2017-11-26 13:04:48 -0600

    added routes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,7 @@ console.log('JS loaded');
 $(document).ready(function(){
     console.log('JQ ready');
     getItems();
-
+//add event listeners here for submit, delete, mark completed***
 
 
 })//end document ready
@@ -32,4 +32,55 @@ function getItems(){
         }
     })
     
+}
+
+
+function addItem(){
+    $.ajax({  //including this in document ready is not a common practice, just for this demo
+        method: 'POST',
+        url: '/todo',
+        data: {
+            item: $('#newListItem').val(),
+            completed: 'N'
+        },
+        success: function(response){
+            console.log('response', response);
+            getItems(); //call get function
+        }
+    })
+}
+
+
+
+function removeItem(){
+    console.log($(this).data());
+    var shoeIdToRemove = $(this).data().id;
+    console.log('remove shoe was clicked! The shoe id was ', shoeIdToRemove);
+
+    $.ajax ({
+        method: 'DELETE',
+        url: '/shoes/' + shoeIdToRemove,
+        success: function(response){
+            getItems(); // call get function
+        }
+    })
+}
+
+
+function editItem(){
+    console.log($(this).data()); // this should love {id:7} or whatever id is  
+    var shoeIdToSave = $(this).data().id;
+    var shoeNameToSave = $(this).parent().children(".newShoeName").val();
+    console.log('save shoe was clicked! The shoe id was ', shoeIdToSave);
+
+    $.ajax({
+        method: 'PUT',
+        url: '/shoes/' + shoeIdToSave,
+        data: {
+            name: shoeNameToSave,
+        },
+        success: function (response) {
+            getItems();
+        }
+    })
 }
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -31,7 +31,87 @@ router.get('/', function(req,res){
 });//end GET todo
 
 
+router.post('/', function (req,res){
+    //Attempt to connect to database
+    pool.connect(function(errorConnectingToDatabase, client, done){
+        if(errorConnectingToDatabase){
+            //There was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // We connected to the database!
+            //Now, we're going to GET things from the DB
+            //use ES6 backtick ` to select multi lines below****
+            client.query(`INSERT INTO todo_list (item, completed)
+            VALUES ($1, $2);`, [req.body.item, req.body.completed], function(errorMakingQuery, result){
+                done();
+                if(errorMakingQuery){
+                    //Query failed.  Did you test it in Postico?
+                    //Log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else{
+                    res.sendStatus(201); //201 is status 'created'
+                }
+            });
+        }
+    });
+})
 
+router.delete('/:id', function (req,res){
+    var shoeIdToRemove = req.params.id;
+    //Attempt to connect to database
+    pool.connect(function(errorConnectingToDatabase, client, done){
+        if(errorConnectingToDatabase){
+            //There was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // We connected to the database!
+            //Now, we're going to GET things from the DB
+            //use ES6 backtick ` to select multi lines below****
+            client.query(`DELETE FROM shoes WHERE id=$1;`, [shoeIdToRemove], function(errorMakingQuery, result){
+                done();
+                if(errorMakingQuery){
+                    //Query failed.  Did you test it in Postico?
+                    //Log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else{
+                    res.sendStatus(200);
+                }
+            });
+        }
+    });
+})
+
+router.put('/:id', function (req,res){
+    var shoeIdToSave = req.params.id;
+    var shoeNameToSave = req.body.name;
+    //Attempt to connect to database
+    pool.connect(function(errorConnectingToDatabase, client, done){
+        if(errorConnectingToDatabase){
+            //There was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // We connected to the database!
+            //Now, we're going to GET things from the DB
+            //use ES6 backtick ` to select multi lines below****
+            client.query(`UPDATE shoes SET "name" = $1 WHERE "id" = $2;`, [shoeNameToSave, shoeIdToSave], function(errorMakingQuery, result){
+                done();
+                if(errorMakingQuery){
+                    //Query failed.  Did you test it in Postico?
+                    //Log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else{
+                    res.sendStatus(200);
+                }
+            });
+        }
+    });
+})
 
 
 
